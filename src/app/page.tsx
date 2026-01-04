31b93e2a974d555921cb49950008c89578066397
Ana Teletanska <ateletanska@yahoo.com>
2026-01-04 20:50:52 +0100

    Render tasks on Today page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
+import { Task } from "@/lib/types";
+import TaskList from "@/components/TaskList";
 export default function Home() {
+  const task1: Task = {
+    id: "1",
+    description: "Do homework",
+    difficulty: 5,
+    priority: 5,
+    completion: "not_completed",
+  };
+
+  const task2: Task = {
+    id: "2",
+    description: "Clean House",
+    difficulty: 7,
+    priority: 10,
+    completion: "not_completed",
+  };
+
+  const tasks: Task[] = [task1, task2];
+
   return (
     <main className="p-6 max-w-3xl mx-auto">
       <h1 className="text-3xl font-bold">Today</h1>
 
       <section className="mt-6">
         <h2 className="text-xl font-semibold">Tasks</h2>
+        <TaskList tasks={tasks}></TaskList>
       </section>
 
       <section className="mt-6">
