b083c12b59cfeb403829ad1063f4114bad11de8c
Ana Teletanska <ateletanska@yahoo.com>
2026-01-04 14:32:37 +0100

    add initial task importance and productivity calculation logic

---
+++ src/lib/scores.ts
@@ -0,0 +1,28 @@
+import {Task} from "./types"
+
+// calculate importance of each task 
+
+// later add option for user to prioritize tasks based on 
+// difficulty or priority and implement different calculation logic
+export const getTaskImportance = (task: Task): number => {
+    const score = (task.difficulty * (task.priority + 10)) / 100;
+    return score;
+}
+
+// calculate overall daily productivity
+const calculateProductivityScore = (tasks: Task[]): number => {
+    if (tasks.length === 0) return 0;
+    let allTaskScore = 0;
+    let completedTaskScore = 0;
+
+    tasks.forEach((task)  => {
+        const taskWeight = getTaskImportance(task);
+
+        allTaskScore += taskWeight;
+
+        if (task.completion === 'completed') {
+            completedTaskScore += taskWeight;
+        } 
+    });
+    return completedTaskScore / allTaskScore;
+}
