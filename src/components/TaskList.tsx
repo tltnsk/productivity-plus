977c4b2cc391e82fac45996c0eeb600e042b5219
Ana Teletanska <ateletanska@yahoo.com>
2026-01-15 00:33:41 +0100

    add comments

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,15 @@
+/**
+ * This is a TaskList component.
+ *   - container for task items
+ *   - manages spacing of tasks
+ *   - passes action handlers to individual tasks
+ */
+
 import { Task } from "@/lib/types";
 import TaskItem from "@/components/TaskItem";
+
 // Accept Task[] as props
 // Render task description in a list
-
 type TaskListProps = {
   tasks: Task[];
   onToggleTask: (taskId: string) => void;
@@ -17,6 +24,7 @@ export default function TaskList({
   return (
     <ul className="space-y-4 mt-5 py-2 px-4">
       {tasks.map((task) => (
+        // unique key to distinguish each task
         <li key={task.id}>
           <TaskItem
             task={task}
