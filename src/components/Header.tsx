cb36696286aa4834b3334b1a05fc9126ac8bb033
Ana Teletanska <ateletanska@yahoo.com>
2026-01-17 23:24:58 +0100

    Change logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,13 @@ import Image from "next/image";
 export function Header() {
   return (
     <div className="flex justify-between">
-      <Image src="/logo-new.png" alt="Logo" width={150} height={150} priority />
+      <Image
+        src="/changed-logo.png"
+        alt="Logo"
+        width={150}
+        height={150}
+        priority
+      />
       <ThemeToggle />
     </div>
   );
