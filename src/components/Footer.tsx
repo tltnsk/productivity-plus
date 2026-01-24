dabfb915e59d9c95937edccc7b07e5d4590ca12e
Ana Teletanska <ateletanska@yahoo.com>
2026-01-24 16:00:04 +0100

    change colors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,17 @@ import { FaGithub } from "react-icons/fa";
 
 export function Footer() {
   const theme = useTheme();
+  const isDark = theme.palette.mode === "dark";
   return (
     <footer className="flex w-full flex-col  items-center justify-center pt-10 gap-x-12">
       <div className="flex flex-row gap-6">
         <a href="https://github.com/tltnsk" target="_blank" rel="Github link">
-          <FaGithub size={20} style={{ color: theme.palette.text.secondary }} />
+          <FaGithub
+            size={20}
+            style={{ color: isDark ? "#1b3ff2" : "#ee3f71" }}
+          />
         </a>
-        <p style={{ color: theme.palette.text.secondary }}>
+        <p style={{ color: isDark ? "#1b3ff2" : "#ee3f71" }}>
           {" "}
           {new Date().getFullYear()}
         </p>
